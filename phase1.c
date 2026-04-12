58ef0a2045de86ccb48d00b7444dabe3d4e32dc2
quikschool <79000571+quikschool@users.noreply.github.com>
2026-04-12 11:08:29 +0400

    Update parseCmds to handle quoting

--- phase1.c
+++ phase1.c
@@ -9,20 +9,29 @@
 
 int parseCmds(char *str, char *result[], int cmdIndex[]) {
 	// result is structured as {arg1, arg2, ..., NULL, arg1, arg2, ..., NULL}
-	int foundCmd=0, cmds=0, wordCount=0, spacing=0, breaking=0;
+	int cmds=0, wordCount=0;
+	int spacing=1, breaking=1, quoting1=0, quoting2=0;
 	int str_size = strlen(str);
 	if (str_size == 0) return -1;
-	if (str[0] == ' ') spacing = 1;
 	for (int i=0; i<str_size; i++) {
-		if (str[i] == ' ') {
+		if (quoting1) {
+			if (str[i] == '"') {
+				quoting1 = 0;
+				str[i] = '\0';
+				spacing = 1;
+			}
+		} else if (quoting2) {
+			if (str[i] == '\'') {
+				quoting2 = 0;
+				str[i] = '\0';
+				spacing = 1;
+			}
+		} else if (str[i] == ' ') {
 			if (!spacing) {
 				str[i] = '\0';
 				spacing = 1;
 			}
 		} else if (str[i] == '|') {
-			// '|' found before any commands
-			if (!foundCmd) return -1;
-			
 			// 2 '|' symbols inputted without a command in between 
 			if (breaking) return -1;
 
@@ -36,18 +45,24 @@ int parseCmds(char *str, char *result[], int cmdIndex[]) {
 			result[wordCount++] = NULL;
 			breaking = 1;
 		} else if (spacing || breaking) {
-			if (breaking || !foundCmd) {
+			if (breaking) {
 				if (cmds == MAXPIPE + 1) return -1;
 				cmdIndex[cmds++] = wordCount;
 				breaking = 0;
 			}
-			foundCmd = 1;
-			result[wordCount++] = &str[i];
+			if (str[i] == '"') {
+				result[wordCount++] = &str[i+1];
+				quoting1 = 1;
+			} else if (str[i] == '\'') {
+				result[wordCount++] = &str[i+1];
+				quoting2 = 1;
+			} else
+				result[wordCount++] = &str[i];
 			spacing = 0;
 		}
 	}
 	if (breaking) return -1;
-	if (!foundCmd) return -1;
+	if (quoting1 || quoting2) return -1;
 	result[wordCount] = NULL;
 	return cmds;
 }
